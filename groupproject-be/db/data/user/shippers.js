cc32ee3f1056a343176363ad3024d24642303f68
hakhanhne <s3877707@rmit.edu.vn>
2023-08-30 16:50:53 +0700

    add models, sample data

---
+++ groupproject-be/db/data/user/shippers.js
@@ -0,0 +1,12 @@
+const shippers = [
+  {
+    user: "s1",
+    hub: "h1"
+  },
+  {
+    user: "s2",
+    hub: "h2"
+  },
+]
+
+module.exports = shippers;
---
+++ groupproject-be/db/data/user/users.js
@@ -0,0 +1,40 @@
+const users = [
+  {
+    _id: "c1",
+    username: "customer1",
+    password: "User123@",
+    role: "customer"
+  },
+  {
+    _id: "c2",
+    username: "customer2",
+    password: "User123@",
+    role: "customer"
+  },
+  {
+    _id: "v1",
+    username: "vendor1",
+    password: "User123@",
+    role: "vendor"
+  },
+  {
+    _id: "v2",
+    username: "vendor2",
+    password: "User123@",
+    role: "vendor"
+  },
+  {
+    _id: "s1",
+    username: "shipper1",
+    password: "User123@",
+    role: "shipper"
+  },
+  {
+    _id: "s2",
+    username: "shipper2",
+    password: "User123@",
+    role: "shipper"
+  },
+]
+
+module.exports = users;
