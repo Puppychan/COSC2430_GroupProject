48073ea6816411d225b8f4f450669094050d304e
hakhanhne <s3877707@rmit.edu.vn>
2023-09-06 18:08:18 +0700

    user functions are available (in db_service)

--- groupproject-be/db/data/user/vendors.js
+++ groupproject-be/db/data/user/vendors.js
@@ -4,14 +4,14 @@ const vendors = [
   {
     _id: new ObjectId('00000001db42a4ac30ff65a1'),
     user: "00000001db42a4ac30ff65a1",
-    business_name: "Vendor 1",
-    business_address: "Vendor 1 address"
+    name: "Vendor 1",
+    address: "Vendor 1 address"
   },
   {
     _id: new ObjectId('00000001db42a4ac30ff65a2'),
     user: "00000001db42a4ac30ff65a2",
-    business_name: "Vendor 2",
-    business_address: "Vendor 2 address"
+    name: "Vendor 2",
+    address: "Vendor 2 address"
   },
 ]
 
